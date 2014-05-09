793d23e8af7dbb1ff90dd7b71b74a2925b7a1906
Ondrej Bima <ondrej.bima@kraxnet.cz>
2014-05-09 17:22:39 +0200

    nextweek.cz working

--- resources/lib/NextWeekClient.py
+++ resources/lib/NextWeekClient.py
@@ -13,32 +13,33 @@ class NextWeekClient(object):
 		self.addon = addon
 		self._t = addon.getLocalizedString
 
-	# def download(self,link):
+	def download(self,link):
 
-	# 	dest_dir = os.path.join(xbmc.translatePath(self.addon.getAddonInfo('profile').decode("utf-8")), 'temp')
-	# 	dest = os.path.join(dest_dir, "download.tmp")
+		dest_dir = os.path.join(xbmc.translatePath(self.addon.getAddonInfo('profile').decode("utf-8")), 'temp')
+		dest = os.path.join(dest_dir, "download.tmp")
 
-	# 	log(__name__,'Downloading subtitles from %s' % link)
-	# 	res = urllib.urlopen(link)
+		log(__name__,'Downloading subtitles from %s' % link)
+		res = urllib.urlopen(link)
 		
-	# 	subtitles_filename = re.search("Content\-Disposition: attachment; filename=\"(.+?)\"",str(res.info())).group(1)
-	# 	log(__name__,'Filename: %s' % subtitles_filename)
-	# 	subtitles_format = re.search("\.(\w+?)$", subtitles_filename, re.IGNORECASE).group(1)
-	# 	log(__name__,"Subs in %s" % subtitles_format)
+		subtitles_filename = re.search("Content\-Disposition: attachment; filename=\"(.+?)\"",str(res.info())).group(1)
+		log(__name__,'Filename: %s' % subtitles_filename)
+		subtitles_format = re.search("\.(\w+?)$", subtitles_filename, re.IGNORECASE).group(1)
+		log(__name__,"Subs in %s" % subtitles_format)
 		
-	# 	subtitles_data = res.read()
+		subtitles_data = res.read()
 
-	# 	log(__name__,'Saving to file %s' % dest)
-	# 	zip_file = open(dest,'wb')
-	# 	zip_file.write(subtitles_data)
-	# 	zip_file.close()
+		log(__name__,'Saving to file %s' % dest)
+		zip_file = open(dest,'wb')
+		zip_file.write(subtitles_data)
+		zip_file.close()
 
-	# 	final_dest = os.path.join(dest_dir, "download." + subtitles_format)
+		final_dest = os.path.join(dest_dir, "download." + subtitles_format)
 
-	# 	log(__name__,'Changing filename to %s' % final_dest)
-	# 	os.rename(dest, final_dest)
+		log(__name__,'Changing filename to %s' % final_dest)
+		os.rename(dest, final_dest)
 
-	# 	return final_dest
+		return final_dest
+
 	def get_tv_show_list(self):
 		res = urllib.urlopen(self.server_url + "/titulky/category/1-titulky")
 		if not res.getcode() == 200: return None
@@ -90,89 +91,82 @@ class NextWeekClient(object):
 	def search(self, item):
 		title = item['mansearchstr'] if item['mansearch'] else item['tvshow']
 
+		if item['3let_language'] and "cze" not in item['3let_language']:
+			dialog = xbmcgui.Dialog()
+			if dialog.yesno(self.addon.getAddonInfo('name'), self._t(32100), self._t(32101)):
+				xbmc.executebuiltin("ActivateWindow(videossettings)")
+			return results_with_stats(None, self.addon, title, item)
+
 		all_tv_show_list = self.get_tv_show_list()
 		if not all_tv_show_list: return results_with_stats(None, self.addon, title, item)
 
 		tvshow_url = self.search_show_url(title, all_tv_show_list)
 		if tvshow_url == None: return results_with_stats(None, self.addon, title, item)
 
-	# 	found_season_subtitles = self.search_season_subtitles(tvshow_url,item['season'])
-	# 	log(__name__, ["Season filter", found_season_subtitles])
+		found_season_subtitles = self.search_season_subtitles(tvshow_url,item['season'])
+		log(__name__, ["Season filter", found_season_subtitles])
+		if found_season_subtitles == None: return results_with_stats(None, self.addon, title, item)
 
-	# 	episode_subtitle_list = self.filter_episode_from_season_subtitles(found_season_subtitles,item['season'],item['episode'])
-	# 	log(__name__, ["Episode filter", episode_subtitle_list])
-	# 	if episode_subtitle_list == None: return results_with_stats(None, self.addon, title, item)
+		episode_subtitle = self.filter_episode_from_season_subtitles(found_season_subtitles,item['season'],item['episode'])
+		log(__name__, ["Episode filter", episode_subtitle])
+		if episode_subtitle == None: return results_with_stats(None, self.addon, title, item)
 
-	# 	lang_filetred_episode_subtitle_list = self.filter_subtitles_by_language(item['3let_language'], episode_subtitle_list)
-	# 	log(__name__, ["Language filter", lang_filetred_episode_subtitle_list])
-	# 	if lang_filetred_episode_subtitle_list == None: return results_with_stats(None, self.addon, title, item)
+		result_subtitles = [{
+			'filename': HTMLParser.HTMLParser().unescape(episode_subtitle['full_title']),
+			'link': self.server_url + episode_subtitle['link'],
+			'lang': episode_subtitle['lang'],
+			'rating': "0",
+			'sync': False,
+			'lang_flag': xbmc.convertLanguage(episode_subtitle['lang'],xbmc.ISO_639_1),
+		}]
 
-	# 	result_subtitles = []
-	# 	for episode_subtitle in lang_filetred_episode_subtitle_list['versions']:
+		log(__name__,["Search result", result_subtitles])
 
-	# 		result_subtitles.append({
-	# 			'filename': HTMLParser.HTMLParser().unescape(lang_filetred_episode_subtitle_list['full_title']),
-	# 			'link': self.server_url + episode_subtitle['link'],
-	# 			'lang': episode_subtitle['lang'],
-	# 			'rating': "0",
-	# 			'sync': False,
-	# 			'lang_flag': xbmc.convertLanguage(episode_subtitle['lang'],xbmc.ISO_639_1),
-	# 		})
+		return results_with_stats(result_subtitles, self.addon, title, item)
 
-	# 	log(__name__,["Search result", result_subtitles])
+	def filter_episode_from_season_subtitles(self, season_subtitles, season, episode):
+		for season_subtitle in season_subtitles:
+			if (season_subtitle['episode'] == int(episode) and season_subtitle['season'] == int(season)):
+				return season_subtitle
+		return None
 
-	# 	return results_with_stats(result_subtitles, self.addon, title, item)
+	def search_season_subtitles(self, show_url, show_series):
+		res = urllib.urlopen(self.server_url + show_url)
+		if not res.getcode() == 200: return None
+		series_list_content = re.search("<section id=\"content\" class=\"grid-block\">(.+?)</section>", res.read(), re.IGNORECASE | re.DOTALL )
+		if series_list_content == None: return None
 
-	# def filter_subtitles_by_language(self, set_languages, subtitles_list):
-	# 	if not set_languages: return subtitles_list
+		selected_serie_url = None
+		series_list = []
+		for series_list_html in re.findall("<div class=\"pd-subcategory\">(.+?)</div>", series_list_content.group(1).decode("utf-8"), re.IGNORECASE | re.DOTALL):
+			serie_url, serie_number = re.search("<a href=\"(.+?)\">([\d]+?)\. .+?</a>", series_list_html, re.IGNORECASE).groups()
+			if serie_number == show_series:
+				selected_serie_url = serie_url
+				break
 
-	# 	log(__name__, ['Filter by languages', set_languages])
-	# 	filter_subtitles_list = []
-	# 	for subtitle in subtitles_list['versions']:
-	# 		if xbmc.convertLanguage(subtitle['lang'],xbmc.ISO_639_2) in set_languages:
-	# 			filter_subtitles_list.append(subtitle)
+		log(__name__, "Selected Serie URL: %s" % selected_serie_url)
+		if selected_serie_url == None: return None
 
-	# 	if not filter_subtitles_list:
-	# 		if "cze" not in set_languages and "slo" not in set_languages:
-	# 			dialog = xbmcgui.Dialog()
-	# 			if dialog.yesno(self.addon.getAddonInfo('name'), self._t(32100), self._t(32101)):
-	# 				xbmc.executebuiltin("ActivateWindow(videossettings)")
-	# 		return None
-	# 	else:
-	# 		filter_results_list = copy.deepcopy(subtitles_list)
-	# 		filter_results_list['versions'] = filter_subtitles_list
-	# 		return filter_results_list
+		res = urllib.urlopen(self.server_url + selected_serie_url)
+		if not res.getcode() == 200: return None
+		subtitles_list_content = re.search("<section id=\"content\" class=\"grid-block\">(.+?)</section>", res.read(), re.IGNORECASE | re.DOTALL )
+		if subtitles_list_content == None: return None
 
-	# def filter_episode_from_season_subtitles(self, season_subtitles, season, episode):
-	# 	for season_subtitle in season_subtitles:
-	# 		if (season_subtitle['episode'] == int(episode) and season_subtitle['season'] == int(season)):
-	# 			return season_subtitle
-	# 	return None
+		subtitles_list = []
+		for subtitles_list_html in re.findall("<div class=\"pd-filebox\">(.+?)Detail</a></div></div>", subtitles_list_content.group(1).decode("utf-8"), re.IGNORECASE | re.DOTALL):
+			subtitle = {}
+			download_url, show_full_title = re.search("<div class=\"pd-float\"><a href=\"(.+?)\" >(.+?)</a></div>", subtitles_list_html, re.IGNORECASE | re.DOTALL).groups()
+			show_title_with_numbers = re.search("(.+?) ([\d]+?)x([\d]+?)$",show_full_title).groups()
+			# subtitle_version = re.search("Verze:&lt;\/div&gt;&lt;div class=\'pd-fl-m\'&gt;(.+?)&lt;\/div&gt;",subtitles_list_html, re.IGNORECASE | re.DOTALL | re.MULTILINE)
+			# if subtitle_version: log("debug", subtitle_version.group(1))
+			subtitle['full_title'] = show_full_title
+			subtitle['season'] = int(show_title_with_numbers[1])
+			subtitle['episode'] = int(show_title_with_numbers[2])
+			subtitle['title'] = show_title_with_numbers[0]
+			subtitle['lang'] = "Czech"
+			subtitle['link'] = download_url
+			subtitles_list.append(subtitle)
 
-	# def search_season_subtitles(self, show_url, show_series):
-	# 	res = urllib.urlopen(self.server_url + show_url + "titulky/?season=" + show_series)
-	# 	if not res.getcode() == 200: return []
-	# 	subtitles = []
-	# 	html_subtitle_table = re.search("<table class=\"episodes\">.+<tbody>(.+?)</tbody>.+</table>",res.read(), re.IGNORECASE | re.DOTALL)
-	# 	if html_subtitle_table == None: return []
-	# 	for html_episode in re.findall("<tr>(.+?)</tr>", html_subtitle_table.group(1), re.IGNORECASE | re.DOTALL):
-	# 		subtitle = {}
-	# 		show_title_with_numbers = re.sub("<[^<]+?>", "",re.search("<h3>(.+?)</h3>", html_episode).group(1))
-	# 		subtitle['full_title'] = show_title_with_numbers
-	# 		show_title_with_numbers = re.search("S([0-9]+)E([0-9]+): (.+)",show_title_with_numbers).groups()
-	# 		subtitle['season'] = int(show_title_with_numbers[0])
-	# 		subtitle['episode'] = int(show_title_with_numbers[1])
-	# 		subtitle['title'] = show_title_with_numbers[2]
-	# 		subtitle['versions'] = []
-	# 		for subs_url, subs_lang in re.findall("a href=\"(.+?)\" class=\"flag\".+?><i class=\"flag\-.+?\">(cz|sk)</i>",html_episode):
-	# 			subtitle_version = {}
-	# 			subtitle_version['link'] = re.sub("/titulky/#content","/titulky/?direct=1",subs_url)
-	# 			subtitle_version['lang'] = subs_lang.upper()
-	# 			if subtitle_version['lang'] == "CZ": subtitle_version['lang'] = "Czech" 
-	# 			if subtitle_version['lang'] == "SK": subtitle_version['lang'] = "Slovak"
-					
-	# 			subtitle['versions'].append(subtitle_version)
-	# 		if len(subtitle['versions']) > 0: subtitles.append(subtitle)
-	# 	return subtitles
+		return subtitles_list
 
 
